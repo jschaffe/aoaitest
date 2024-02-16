adc7175c2d9c9017978b5fc3d7888a1214f1d945
James Schaffer <jschaffe@microsoft.com>
2024-02-16 10:13:23 -0500

    Change endpoint back

--- Helpers/OpenAIHelper.cs
+++ Helpers/OpenAIHelper.cs
@@ -14,10 +14,10 @@ namespace aoaifunctest.Helpers
         }
         //static string key = "9f4d0240980a416c9ac4996342e7bc43";
         //static string endpoint = "https://jschaffeopenai.openai.azure.com/";
-        //static string key = "bced092b215a4768bcabe21028e00719";
-        //static string endpoint = "https://openaikesteph.openai.azure.com/";
-        static string key = "872e38c195a74f5986d02c37ef10b85d";
-        static string endpoint = "https://openai-rapid.openai.azure.us/";
+        static string key = "bced092b215a4768bcabe21028e00719";
+        static string endpoint = "https://openaikesteph.openai.azure.com/";
+        //static string key = "872e38c195a74f5986d02c37ef10b85d";
+        //static string endpoint = "https://openai-rapid.openai.azure.us/";
         string completion = "";
 
         public string GetPromptResponse(string prompt)
@@ -29,8 +29,8 @@ namespace aoaifunctest.Helpers
                 {
                     CompletionsOptions completionsOptions = new CompletionsOptions
                     {
-                        DeploymentName = "gpt-4-1106preview",
-                        //DeploymentName = "davinci-summary",
+                        //DeploymentName = "gpt-4-1106preview",
+                        DeploymentName = "davinci-summary",
                         //DeploymentName = "gpt35turbo",
                         Prompts = { prompt },
                         MaxTokens = 1000
